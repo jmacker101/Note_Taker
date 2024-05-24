ef669e90647408ba26ec708aa6d7cdc1518d52f5
jmacker101 <jm470t@gmail.com>
2024-05-24 15:04:39 -0400

    fixed routes

--- API Routes/htmlRoutes.js
+++
@@ -1,9 +0,0 @@
-// Route to serve the notes.html file
-app.get("/notes.html", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/notes.html"));
-});
-
-// Route to serve the index.html file
-app.get("\public\index.html", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/index.html"));
-});
